0f07ff6a2d03ff1b5475cd57e875d1d0a236828e
GloriaJe <gloria.ejjennane@gmail.com>
2017-03-07 09:03:27 +0100

    album listage photos

--- controllers/AlbumController.js
+++ controllers/AlbumController.js
@@ -1,8 +1,25 @@
-
 // ////////////////////// L I S T E R     A L B U M S
-var model = require("../models/vip.js");
+var model = require("../models/album.js");
 
-module.exports.ListerAlbum = 	function(request, response){
-   response.title = 'Album des stars';
-   response.render('listerAlbum', response);
-} ;
+module.exports.ListerAlbum = function(request, response){
+    //var lettreVip = request.params.lettreVip;
+    response.title = 'Album des stars';
+    var async = require('async');
+    async.parallel([
+        function(callback){
+            model.photoPremierVip(function(err, result){callback(null, result)});
+        },
+        function(callback){
+            model.photo1(function(err, result){callback(null, result)});
+        },
+    ],
+    function(err, result){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        response.premierePhoto = result[0][0]; 
+        response.photos = result[1]; 
+        response.render('listerAlbum', response);
+    });
+}
--- models/album.js
+++ models/album.js
@@ -1,12 +1,25 @@
 var db = require('../configDb');
 
 /**
- * Cette fonction recupère les albums des vips.
+ * Cette fonction recupère l'adresse des photos 1 de chaque vip par ordre alphabétique.
  */
-module.exports.vips = function(callback) {
+module.exports.photo1 = function(callback) {
     db.getConnection(function(err, connexion) {
         if (!err) {
-            var sql = "SELECT DISTINCT VIP_NUMERO, VIP_PRENOM, VIP_NOM FROM VIP ORDER BY VIP_NOM;";
+            var sql = "SELECT VIP_NOM, VIP_PRENOM, PHOTO_ADRESSE FROM photo p JOIN vip v ON p.VIP_NUMERO = v.VIP_NUMERO WHERE PHOTO_NUMERO = 1 ORDER BY VIP_NOM";
+            connexion.query(sql, callback);
+            connexion.release();
+        }
+    });
+};
+
+/**
+ * Cette fonction recupère l'adresse de la première photo du premier VIP.
+ */
+module.exports.photoPremierVip = function(callback) {
+    db.getConnection(function(err, connexion) {
+        if (!err) {
+            var sql = "SELECT VIP_NOM, VIP_PRENOM, PHOTO_ADRESSE FROM photo p JOIN vip v ON p.VIP_NUMERO = v.VIP_NUMERO WHERE PHOTO_NUMERO = 1 ORDER BY VIP_NOM LIMIT 1 OFFSET 0";
             connexion.query(sql, callback);
             connexion.release();
         }
